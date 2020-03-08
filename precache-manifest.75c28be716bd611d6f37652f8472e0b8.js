603af3647be9c310d72f03c6ff49a7839095752e
Yonatan Tin <yonitint1@gmail.com>
2020-03-08 16:51:02 +0200

    Updates

---
+++ precache-manifest.75c28be716bd611d6f37652f8472e0b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe864afd1dcab60a5519792abb490a9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bea209c1a5ac347de9b3",
+    "url": "/monsters-rolodex/static/css/main.4b44bd16.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea209c1a5ac347de9b3",
+    "url": "/monsters-rolodex/static/js/main.ef96c0f0.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
